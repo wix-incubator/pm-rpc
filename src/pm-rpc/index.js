70bfc784c6809bbb5aa7dc20d3a79945f0fbb075
Omer Ganim <ganimomer@gmail.com>
2017-06-18 10:21:35 +0300

    add Array.from polyfill

--- src/pm-rpc/index.js
+++ src/pm-rpc/index.js
@@ -1,5 +1,10 @@
 import * as api from './rpc'
 import es6Promise from 'es6-promise'
+import arrayFrom from 'array-from'
+
+if (!Array.from) {
+    Array.from = arrayFrom
+}
 es6Promise.polyfill()
 
 export {
