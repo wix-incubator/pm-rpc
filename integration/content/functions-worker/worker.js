a0a6e5279e19d32a7bd155fcaa7f548ecf2b1b2d
Omer Ganim <ganimomer@gmail.com>
2017-09-17 12:40:14 +0300

    fix eslint

--- integration/content/functions-worker/worker.js
+++ integration/content/functions-worker/worker.js
@@ -1,11 +1,10 @@
 import * as pmrpc from '../../../src/pm-rpc/index'
 const api = {
   reduce(fn, initial, arr) {
-    return arr.reduce(function (p, i) {
-      return p.then(function (acc) {
-        return fn(acc, i)
-      })
-    }, Promise.resolve(initial))
+    return arr.reduce((p, i) =>
+      p.then(acc =>
+        fn(acc, i)),
+      Promise.resolve(initial))
   }
 }
 pmrpc.api.set('worker-functions', api)
--- integration/functions-worker.spec.js
+++ integration/functions-worker.spec.js
@@ -7,7 +7,7 @@ describe('passing functions', () => {
     pmrpc.api.request('worker-functions', {target: worker})
       .then(api => {
         function add(a, b) {
-          return a + b;
+          return a + b
         }
         api.reduce(add, 0, [1, 2, 3, 4, 5])
           .then(res => {
