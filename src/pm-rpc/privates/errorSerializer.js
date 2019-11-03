bbcafbe42965d6ba7ac780145745020498d6dbbd
talm <talm@wix.com>
2019-11-03 18:16:03 +0200

    remove use in isNative

--- src/pm-rpc/privates/errorSerializer.js
+++ src/pm-rpc/privates/errorSerializer.js
@@ -1,14 +1,13 @@
 import get from 'lodash/get'
 import isError from 'lodash/isError'
-import isNative from 'lodash/isNative'
 
 const isErrorType = ({type} = {}) => type === 'Error' || isError(get(self, [type, 'prototype']))
 const toError = ({type, stack, message}) => {
-  const err = new self[type](message)
-  err.stack = stack
-  return err
+    const err = new (self[type] || Error)(message)
+    err.stack = stack
+    return err
 }
 
-const toErrorInfo = ({constructor: {name}, message, stack}) => ({type: isNative(self[name]) ? name : 'Error', message, stack})
+const toErrorInfo = ({constructor: {name: type}, message, stack}) => ({type, message, stack})
 export const serialize = err => Promise.reject(isError(err) ? toErrorInfo(err) : err)
 export const deserialize = err => isErrorType(err) ? toError(err) : err
