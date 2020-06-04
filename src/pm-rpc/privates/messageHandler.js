96b2ce3b9cba207280fb2cdc5d7e48ef46c1d31d
Vlada Leykin <vladale@wix.com>
2020-06-04 16:36:14 +0300

    2.0.2 trigger worker.addEventListener on each rpc.set

--- src/pm-rpc/privates/messageHandler.js
+++ src/pm-rpc/privates/messageHandler.js
@@ -1,12 +1,12 @@
-
 let isListening = false
 export const addSingleHandler = (handler, workers) => {
   if (!isListening) {
     isListening = true
     self.addEventListener('message', handler)
-    if (workers) {
+  }
+
+  if (workers) {
       workers.forEach(worker => worker.addEventListener('message', handler))
-    }
   }
 }
 
