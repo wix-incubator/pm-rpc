99488be5373b03d01ab213c3bcb0219fafdec227
Amit Avihud <amitav@wix.com>
2023-05-14 12:30:36 +0300

    handle removeSingleHandler

--- src/pm-rpc/privates/messageHandler.js
+++ src/pm-rpc/privates/messageHandler.js
@@ -2,16 +2,26 @@ import isFunction from 'lodash/isFunction'
 
 let isListening = false
 
+const handlersForObj = new WeakMap()
+const registerHandlerForOnmessage = (obj, handler) => {
+  if (!handlersForObj.has(obj)) {
+    handlersForObj.set(obj, [])
+  }
+  handlersForObj.get(obj).push(handler)
+}
+
 const registerListener = (obj, handler) => {
   if (isFunction(obj.addEventListener)) {
     obj.addEventListener('message', handler)
   } else {
-    obj.onmessage = handler
+    if (!handlersForObj.has(obj)) {
+      obj.onmessage = e => (handlersForObj.get(obj) || []).forEach(_handler => _handler(e))
+    }
+    registerHandlerForOnmessage(obj, handler)
   }
 }
 
 export const addSingleHandler = (handler, workers) => {
-  console.log('amit: addSingleHandler')
   if (!isListening) {
     isListening = true
     // todo: consider having this subscription long-living (now we subscribe on the first `set`/`request` and unsubscribe on the last `unset`
@@ -26,6 +36,16 @@ export const addSingleHandler = (handler, workers) => {
 }
 
 export const removeSingleHandler = handler => {
-  self.removeEventListener('message', handler)
+  if (isFunction(self.removeEventListener)) {
+    self.removeEventListener('message', handler)
+  } else {
+    const handlers = handlersForObj.get(self)
+    if (handlers) {
+      const index = handlers.indexOf(handler)
+      if (index !== -1) {
+        handlers.splice(index, 1)
+      }
+    }
+  }
   isListening = false
 }
