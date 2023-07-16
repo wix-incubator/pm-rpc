dffcabe8b3aac5b2e77059c77ed99b4712429d2e
Amit Avihud <amitav@wix.com>
2023-07-16 15:15:04 +0300

    make lint work

--- integration/node/node-worker-threads.js
+++ integration/node/node-worker-threads.js
@@ -8,8 +8,8 @@ const random = Math.random()
 const worker = new Worker(path.resolve(__dirname, './worker.js'))
 
 pmrpc.api.set('api-from-main', {
-    exampleFromMain: () => 'example from main' + random
-}, { workers: [worker]})
+    exampleFromMain: () => `example from main ${random}`
+}, {workers: [worker]})
 
 let workerApi
 pmrpc.api.request('api-from-worker', {target: worker})
@@ -17,17 +17,13 @@ pmrpc.api.request('api-from-worker', {target: worker})
         console.log('api from worker', api)
         workerApi = api
     })
-    .then(() => {
-        return workerApi.example('arg-from-main').then(result => {
-            if (result === 'worker#example was called witharg-from-main; value from main: example from main' + random) {
+    .then(() => workerApi.example('arg-from-main').then(result => {
+            if (result === `worker#example was called witharg-from-main; value from main: example from main ${random}`) {
                 console.log('test passed')
                 process.exitCode = 0
             }
-        })
-    })
-    .finally(() => {
-        return worker.terminate()
-    })
+        }))
+    .finally(() => worker.terminate())
     .then(() => {
         pmrpc.api.unset()
         console.log('after unset')
--- integration/node/worker.js
+++ integration/node/worker.js
@@ -2,13 +2,7 @@ const {parentPort} = require('worker_threads')
 const pmrpc = require('../../build/pm-rpc')
 
 pmrpc.api.set('api-from-worker', {
-    example: (arg) => {
-        return pmrpc.api.request('api-from-main', {target: parentPort})
-            .then(async api => {
-                return api.exampleFromMain()
-            })
-            .then(valueFromMain => {
-                return 'worker#example was called with' + arg + '; value from main: ' + valueFromMain
-            })
-    }
+    example: arg => pmrpc.api.request('api-from-main', {target: parentPort})
+            .then(async api => api.exampleFromMain())
+            .then(valueFromMain => `worker#example was called with${arg}; value from main: ${valueFromMain}`)
 })
