95ac6c0683df6459173e229f52629983ae511ed3
Amit Avihud <amitav@wix.com>
2023-07-23 14:58:33 +0300

    Use __non_webpack_require__ instead of require for worker_threads

--- src/pm-rpc/privates/messageManager.js
+++ src/pm-rpc/privates/messageManager.js
@@ -2,11 +2,11 @@ import {isWebWorker, isBrowser} from './windowModule'
 import cloneDeep from 'lodash/cloneDeep'
 
 // eslint-disable-next-line no-undef, lodash/prefer-lodash-typecheck, no-use-before-define
-const MessageChannel = typeof globalThis.MessageChannel !== 'undefined' ? globalThis.MessageChannel : require('worker_threads').MessageChannel
+const MessageChannel = typeof globalThis.MessageChannel !== 'undefined' ? globalThis.MessageChannel : __non_webpack_require__('worker_threads').MessageChannel
 // eslint-disable-next-line no-undef, lodash/prefer-lodash-typecheck, no-use-before-define
-const Worker = typeof globalThis.Worker !== 'undefined' ? globalThis.Worker : require('worker_threads').Worker
+const Worker = typeof globalThis.Worker !== 'undefined' ? globalThis.Worker : __non_webpack_require__('worker_threads').Worker
 // eslint-disable-next-line no-undef, lodash/prefer-lodash-typecheck, no-use-before-define
-const MessagePort = typeof globalThis.MessagePort !== 'undefined' ? globalThis.MessagePort : require('worker_threads').MessagePort
+const MessagePort = typeof globalThis.MessagePort !== 'undefined' ? globalThis.MessagePort : __non_webpack_require__('worker_threads').MessagePort
 
 function postMessage(target, message, targetOrigin, transfer) {
   if (isWebWorker() || target instanceof Worker || target instanceof MessagePort) {
