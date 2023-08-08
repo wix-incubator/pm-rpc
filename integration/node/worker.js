0e65571aef149a6a67243a74a250e9b3211446cb
Amit Avihud <amitav@wix.com>
2023-08-08 14:42:58 +0300

    update node int tests

--- integration/node/worker.js
+++ integration/node/worker.js
@@ -1,8 +1,7 @@
-const {parentPort} = require('worker_threads')
 const pmrpc = require('../../')
 
 pmrpc.api.set('api-from-worker', {
-    example: arg => pmrpc.api.request('api-from-main', {target: parentPort})
+    example: arg => pmrpc.api.request('api-from-main')
             .then(async api => api.exampleFromMain())
             .then(valueFromMain => `worker#example was called with${arg}; value from main: ${valueFromMain}`)
 })
